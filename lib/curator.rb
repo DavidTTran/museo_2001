27f86d9fc47b914be1080ad916603e22d0c68fd0
David Tran <davidttemail@gmail.com>
2020-03-04 09:47:29 -0700

    Add tests and methods for sorting photographs by artists

--- lib/curator.rb
+++ lib/curator.rb
@@ -17,4 +17,15 @@ class Curator
   def find_artist_by_id(id)
     @artists.find { |artist| artist.id == id }
   end
+
+  def find_photographs_by_artist(artist)
+    @photographs.find_all { |photograph| photograph.artist_id == artist.id }
+  end
+
+  def photographs_by_artist
+    @artists.reduce({}) do |acc, artist|
+      acc[artist] = find_photographs_by_artist(artist).flatten
+      acc
+    end
+  end
 end
--- test/curator_test.rb
+++ test/curator_test.rb
@@ -9,6 +9,51 @@ class CuratorTest < Minitest::Test
 
   def setup
     @curator = Curator.new
+    @artist_1 = Artist.new({
+      id: "1",
+      name: "Henri Cartier-Bresson",
+      born: "1908",
+      died: "2004",
+      country: "France"
+    })
+    @artist_2 = Artist.new({
+      id: "2",
+      name: "Ansel Adams",
+      born: "1902",
+      died: "1984",
+      country: "United States"
+    })
+    @artist_3 = Artist.new({
+      id: "3",
+      name: "Diane Arbus",
+      born: "1923",
+      died: "1971",
+      country: "United States"
+    })
+    @photo_1 = Photograph.new({
+      id: "1",
+      name: "Rue Mouffetard, Paris (Boy withBottles)",
+      artist_id: "1",
+      year: "1954"
+    })
+      @photo_2 = Photograph.new({
+      id: "2",
+      name: "Moonrise, Hernandez",
+      artist_id: "2",
+      year: "1941"
+    })
+    @photo_3 = Photograph.new({
+      id: "3",
+      name: "Identical Twins, Roselle, NewJersey",
+      artist_id: "3",
+      year: "1967"
+    })
+    @photo_4 = Photograph.new({
+      id: "4",
+      name: "Monolith, The Face of Half   Dome",
+      artist_id: "3",
+      year: "1927"
+    })
   end
 
   def test_it_exists
@@ -21,44 +66,17 @@ class CuratorTest < Minitest::Test
   end
 
   def test_it_can_add_photographs
-    photo_1 = Photograph.new({
-     id: "1",
-     name: "Rue Mouffetard, Paris (Boy with Bottles)",
-     artist_id: "1",
-     year: "1954"
-  })
+  @curator.add_photograph(@photo_1)
+  @curator.add_photograph(@photo_2)
 
-    photo_2 = Photograph.new({
-     id: "2",
-     name: "Moonrise, Hernandez",
-     artist_id: "2",
-     year: "1941"
-  })
-  @curator.add_photograph(photo_1)
-  @curator.add_photograph(photo_2)
-
-  assert_equal [photo_1, photo_2], @curator.photographs
+  assert_equal [@photo_1, @photo_2], @curator.photographs
   end
 
   def test_it_can_add_artists
-    artist_1 = Artist.new({
-      id: "1",
-      name: "Henri Cartier-Bresson",
-      born: "1908",
-      died: "2004",
-      country: "France"
-    })
-    artist_2 = Artist.new({
-      id: "2",
-      name: "Ansel Adams",
-      born: "1902",
-      died: "1984",
-      country: "United States"
-    })
-    @curator.add_artist(artist_1)
-    @curator.add_artist(artist_2)
+    @curator.add_artist(@artist_1)
+    @curator.add_artist(@artist_2)
 
-    assert_equal [artist_1, artist_2], @curator.artists
+    assert_equal [@artist_1, @artist_2], @curator.artists
   end
 
   def test_it_can_find_artists_by_id
@@ -76,10 +94,38 @@ class CuratorTest < Minitest::Test
       died: "1984",
       country: "United States"
     })
-    @curator.add_artist(artist_1)
-    @curator.add_artist(artist_2)
+    @curator.add_artist(@artist_1)
+    @curator.add_artist(@artist_2)
 
-    assert_equal artist_1, @curator.find_artist_by_id("1")
+    assert_equal @artist_1, @curator.find_artist_by_id("1")
+  end
+
+  def test_it_can_find_photographs_by_artist
+    @curator.add_artist(@artist_1)
+    @curator.add_artist(@artist_2)
+    @curator.add_photograph(@photo_1)
+    @curator.add_photograph(@photo_2)
+
+    assert_equal [@photo_1], @curator.find_photographs_by_artist(@artist_1)
+  end
+
+  def test_it_can_sort_photographs_by_artist
+    @curator.add_artist(@artist_1)
+    @curator.add_artist(@artist_2)
+    @curator.add_artist(@artist_3)
+    @curator.add_photograph(@photo_1)
+    @curator.add_photograph(@photo_2)
+    @curator.add_photograph(@photo_3)
+    @curator.add_photograph(@photo_4)
+
+    expected =
+    {
+      @artist_1 => [@photo_1],
+      @artist_2 => [@photo_2],
+      @artist_3 => [@photo_3, @photo_4]
+    }
+
+    assert_equal expected, @curator.photographs_by_artist
   end
 
 

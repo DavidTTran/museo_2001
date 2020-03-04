70153b84a9adb30bdc8db4e02c72edaea1b9e228
David Tran <davidttemail@gmail.com>
2020-03-04 09:24:48 -0700

    Add tests and methods to add artists

--- test/curator_test.rb
+++ test/curator_test.rb
@@ -17,6 +17,7 @@ class CuratorTest < Minitest::Test
 
   def test_it_has_attributes
     assert_equal [], @curator.photographs
+    assert_equal [], @curator.artists
   end
 
   def test_it_can_add_photographs
@@ -38,6 +39,28 @@ class CuratorTest < Minitest::Test
 
   assert_equal [photo_1, photo_2], @curator.photographs
   end
+
+  def test_it_can_add_artists
+    artist_1 = Artist.new({
+    id: "1",
+    name: "Henri Cartier-Bresson",
+    born: "1908",
+    died: "2004",
+    country: "France"
+  })
+  artist_2 = Artist.new({
+    id: "2",
+    name: "Ansel Adams",
+    born: "1902",
+    died: "1984",
+    country: "United States"
+  })
+  @curator.add_artist(artist_1)
+  @curator.add_artist(artist_2)
+
+  assert_equal [artist_1, artist_2], @curator.artists
+  end
+
 
 
 end

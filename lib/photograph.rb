065e57f315f0fd4b59859e80a04a07ea150a0252
David Tran <davidttemail@gmail.com>
2020-03-04 09:07:29 -0700

    Write photograph class

--- lib/photograph.rb
+++ lib/photograph.rb
@@ -1,1 +1,11 @@
+class Photograph
+  attr_reader :name, :id, :artist_id, :year
 
+  def initialize(attributes)
+    @name = attributes[:name]
+    @id = attributes[:id]
+    @artist_id = attributes[:artist_id]
+    @year = attributes[:year]
+  end
+
+end
